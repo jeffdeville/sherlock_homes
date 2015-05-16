66d3e65813d9545ed22d7b11c471469e24903bc8
Davide Papagni <davide@algonauti.com>
2015-05-16 12:50:04 +0200

    use sauce as Yasf's capybara driver for tests

--- spec/support/yasf.rb
+++ spec/support/yasf.rb
@@ -5,6 +5,7 @@ RSpec.configure do |config|
       #config.proxy_host = '127.0.0.1'
       config.timeout = 100
       config.debug = false
+      config.capybara_driver = :sauce
     end
   end
 end
