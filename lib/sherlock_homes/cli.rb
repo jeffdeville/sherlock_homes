10c2ab402c0a36ee24dd0ce9a70851187d6c55ba
Salvatore Ferrucci <salvatore@algonauti.com>
2015-06-15 13:31:44 +0200

    check presence of zillow key

--- lib/sherlock_homes/cli.rb
+++ lib/sherlock_homes/cli.rb
@@ -1,7 +1,7 @@
 module SherlockHomes
   class CLI
     def self.run(address)
-      SherlockHomes::Pipeline.new(raw_location: address).run
+      puts SherlockHomes::Pipeline.new(raw_location: address).run.context.property.inspect
     end
   end
 end
